3ca9ce18781c1efd47250085cf6bc15c210b8727
PhucKhanh <you@example.com>
2025-10-04 12:09:39 +0700

    24020178

---
+++ HW1/Introduction.java
@@ -0,0 +1,20 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Trần Phúc Khánh" + '\t' +"24020178" + '\t' + "I-IT6" + '\t' + "phuckhan123" + "24020178@vnu.edu.vn");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.\n" +
+                "\n" +
+                "Take one down, pass it around, \n" +
+                "\n" +
+                "8 bottles of beer on the wall, 8 bottles of beer.\n" +
+                "\n" +
+                "Take one down, pass it around,\n" +
+                "\n" +
+                "...\n" +
+                "\n" +
+                "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                "\n" +
+                "Take one down, pass it around,\n" +
+                "\n" +
+                "No more bottles of beer on the wall.");
+    }
+}
---
+++ HW1/LeapYear.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if(year % 4 == 0 && year % 400 == 0){
+            System.out.println("LeapYear");
+        }
+        else if(year % 4 == 0 && year % 100 == 0){
+            System.out.println("Not LeapYear");
+        }
+        else if( year % 4 == 0 ){
+            System.out.println("LeapYear");
+        }
+        else{
+            System.out.println("Not LeapYear");
+        }
+    }
+}
